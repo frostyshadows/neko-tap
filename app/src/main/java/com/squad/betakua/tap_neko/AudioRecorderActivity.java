3d49f80e7f301d2fd8cf4eb84572f6f33f13ba77
Sherry Yuan <frostyyshadows@gmail.com>
2019-01-26 14:01:55 -0800

    added developer panel as MainActivity

---
+++ app/src/main/java/com/squad/betakua/tap_neko/AudioRecorderActivity.java
@@ -0,0 +1,17 @@
+package com.squad.betakua.tap_neko;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sherryuan on 2019-01-26.
+ */
+
+public class AudioRecorderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_recorder);
+    }
+}
--- app/src/main/java/com/squad/betakua/tap_neko/MainActivity.java
+++ app/src/main/java/com/squad/betakua/tap_neko/MainActivity.java
@@ -1,13 +1,31 @@
 package com.squad.betakua.tap_neko;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button audioRecorderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initAudioRecorderButton();
+    }
+
+    private void initAudioRecorderButton() {
+        audioRecorderButton = findViewById(R.id.audio_recorder_button);
+        audioRecorderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent audioRecorderIntent = new Intent(getApplicationContext(), AudioRecorderActivity.class);
+                startActivity(audioRecorderIntent);
+            }
+        });
     }
 }
