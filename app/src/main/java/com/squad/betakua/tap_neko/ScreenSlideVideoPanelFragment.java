de9ad47c96229592d2600d63d1fe1c5dc084a59c
Sherry Yuan <frostyyshadows@gmail.com>
2019-01-27 11:59:46 -0800

    added basic notifications

--- app/src/main/java/com/squad/betakua/tap_neko/ScreenSlideVideoPanelFragment.java
+++ app/src/main/java/com/squad/betakua/tap_neko/ScreenSlideVideoPanelFragment.java
@@ -1,5 +1,6 @@
 package com.squad.betakua.tap_neko;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import com.squad.betakua.tap_neko.notifications.AlarmReceiver;
+import com.squad.betakua.tap_neko.notifications.NotificationScheduler;
+
+import java.util.Calendar;
 
 public class ScreenSlideVideoPanelFragment extends Fragment {
 
@@ -28,6 +35,13 @@ public class ScreenSlideVideoPanelFragment extends Fragment {
         FloatingActionButton callFab = getView().findViewById(R.id.callFab);
         FloatingActionButton alertFab = getView().findViewById(R.id.alertFab);
 
+        alertFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePickerDialog();
+            }
+        });
+
         callFab.setVisibility(View.INVISIBLE);
         alertFab.setVisibility(View.INVISIBLE);
 
@@ -45,6 +59,22 @@ public class ScreenSlideVideoPanelFragment extends Fragment {
 
         });
 
+    }
+
+    private void showTimePickerDialog() {
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                NotificationScheduler.setReminder(getContext(),AlarmReceiver.class,
+                        selectedHour, selectedMinute, "title", "dosage");
+            }
+        }, hour, minute, true);//Yes 24 hour time
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
     }
 
 
---
+++ app/src/main/java/com/squad/betakua/tap_neko/notifications/NotificationScheduler.java
@@ -0,0 +1,98 @@
+package com.squad.betakua.tap_neko.notifications;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.Calendar;
+
+import static android.content.Context.ALARM_SERVICE;
+
+/**
+ * Created by sherryuan on 2018-09-16.
+ */
+
+public class NotificationScheduler {
+    public static int DAILY_REMINDER_REQUEST_CODE = 24141;
+    public static void setReminder(Context context, Class<?> cls, int hour, int min, String title, String dosage) {
+        Calendar calendar = Calendar.getInstance();
+        Calendar setcalendar = Calendar.getInstance();
+        setcalendar.set(Calendar.HOUR_OF_DAY, hour);
+        setcalendar.set(Calendar.MINUTE, min);
+        setcalendar.set(Calendar.SECOND, 0);
+        // cancel already scheduled reminders
+        cancelReminder(context,cls);
+
+        if(setcalendar.before(calendar))
+            setcalendar.add(Calendar.DATE,1);
+
+        // Enable a receiver
+        ComponentName receiver = new ComponentName(context, cls);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+
+        Intent intent1 = new Intent(context, cls);
+        intent1.putExtra("title", title);
+        intent1.putExtra("dosage", dosage);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                DAILY_REMINDER_REQUEST_CODE, intent1,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        am.setInexactRepeating(AlarmManager.RTC_WAKEUP, setcalendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
+
+    public static void showNotification(Context context,Class<?> cls,String title) {
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Intent notificationIntent = new Intent(context, cls);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(cls);
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(
+                DAILY_REMINDER_REQUEST_CODE,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "koshu");
+        Notification notification = builder
+                .setContentTitle("Reminder")
+                .setContentText(title)
+                .setAutoCancel(true)
+                .setSound(alarmSound)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentIntent(pendingIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(DAILY_REMINDER_REQUEST_CODE, notification);
+    }
+
+    public static void cancelReminder(Context context,Class<?> cls) {
+        // Disable a receiver
+        ComponentName receiver = new ComponentName(context, cls);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+
+        Intent intent1 = new Intent(context, cls);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                DAILY_REMINDER_REQUEST_CODE, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        am.cancel(pendingIntent);
+        pendingIntent.cancel();
+    }
+}
