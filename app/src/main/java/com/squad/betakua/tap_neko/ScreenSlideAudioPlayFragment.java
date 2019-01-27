32c29b978ec8e8423af91bb8bd0446f480238623
SophieMBerger <berger.m.sophie@gmail.com>
2019-01-27 05:56:58 -0800

    all float buttons

--- app/src/main/java/com/squad/betakua/tap_neko/ScreenSlideAudioPlayFragment.java
+++ app/src/main/java/com/squad/betakua/tap_neko/ScreenSlideAudioPlayFragment.java
@@ -2,6 +2,7 @@ package com.squad.betakua.tap_neko;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,6 +33,27 @@ public class ScreenSlideAudioPlayFragment extends Fragment {
                 playToPauseAudioIcon.playAnimation();
                 //Play audio file
             }
+        });
+
+        FloatingActionButton mainFab = getView().findViewById(R.id.mainFab);
+        FloatingActionButton callFab = getView().findViewById(R.id.callFab);
+        FloatingActionButton alertFab = getView().findViewById(R.id.alertFab);
+
+        callFab.setVisibility(View.INVISIBLE);
+        alertFab.setVisibility(View.INVISIBLE);
+
+        mainFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callFab.getVisibility()==View.VISIBLE){
+                    callFab.setVisibility(View.INVISIBLE);
+                    alertFab.setVisibility(View.INVISIBLE);
+                } else{
+                    callFab.setVisibility(View.VISIBLE);
+                    alertFab.setVisibility(View.VISIBLE);
+                }
+            }
+
         });
     }
 
