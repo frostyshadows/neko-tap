102d90df70872b4a4e856937f665e90bc2f027a9
SophieMBerger <berger.m.sophie@gmail.com>
2019-01-27 11:41:45 -0800

    video issue

--- app/src/main/java/com/squad/betakua/tap_neko/ScreenSlideVideoPanelFragment.java
+++ app/src/main/java/com/squad/betakua/tap_neko/ScreenSlideVideoPanelFragment.java
@@ -1,14 +1,26 @@
 package com.squad.betakua.tap_neko;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.VideoView;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE;
 
 public class ScreenSlideVideoPanelFragment extends Fragment {
 
@@ -33,23 +45,28 @@ public class ScreenSlideVideoPanelFragment extends Fragment {
 
         //VideoPlayer
         VideoView videoView = getView().findViewById(R.id.videoView);
-        // videoView.setVideoURI();
 
-        videoView.setOnClickListener(new View.OnClickListener() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.INTERNET}, REQUEST_CODE);
+        } else {
+            Uri uri = Uri.parse("https://www.youtube.com/watch?v=a1sn_UlUOio");
+            videoView.setVideoURI(uri);
+            videoView.start();
+            videoView.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                DisplayMetrics metrics = new DisplayMetrics();
-                getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-                android.widget.LinearLayout.LayoutParams params = (android.widget.LinearLayout.LayoutParams) videoView.getLayoutParams();
-                params.width =  metrics.widthPixels;
-                params.height = metrics.heightPixels;
-                params.leftMargin = 0;
-                videoView.setLayoutParams(params);
-            }
+                @Override
+                public void onClick(View view) {
+                    DisplayMetrics metrics = new DisplayMetrics();
+                    getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                    android.widget.LinearLayout.LayoutParams params = (android.widget.LinearLayout.LayoutParams) videoView.getLayoutParams();
+                    params.width = metrics.widthPixels;
+                    params.height = metrics.heightPixels;
+                    params.leftMargin = 0;
+                    videoView.setLayoutParams(params);
+                }
 
-        });
-
+            });
+        }
         callFab.setVisibility(View.INVISIBLE);
         alertFab.setVisibility(View.INVISIBLE);
 
