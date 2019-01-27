fe59c2909aa8a409a6a4948ba2911bd8c519a771
SophieMBerger <berger.m.sophie@gmail.com>
2019-01-26 23:11:22 -0800

    fragments created

---
+++ app/src/main/java/com/squad/betakua/tap_neko/ScreenSlideAudioPlayFragment.java
@@ -0,0 +1,19 @@
+package com.squad.betakua.tap_neko;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ScreenSlideAudioPlayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_audio_panel, container, false);
+
+        return rootView;
+    }
+}
