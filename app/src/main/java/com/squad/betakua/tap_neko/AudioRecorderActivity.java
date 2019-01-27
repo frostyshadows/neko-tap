7e8833470f289c5294f9e9ffc4a2e2c4932dec65
SophieMBerger <berger.m.sophie@gmail.com>
2019-01-26 18:13:19 -0800

    audio conversion

--- app/src/main/java/com/squad/betakua/tap_neko/AudioRecorderActivity.java
+++ app/src/main/java/com/squad/betakua/tap_neko/AudioRecorderActivity.java
@@ -1,5 +1,6 @@
 package com.squad.betakua.tap_neko;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -9,7 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
+import cafe.adriel.androidaudioconverter.callback.IConvertCallback;
+import cafe.adriel.androidaudioconverter.model.AudioFormat;
 
 /**
  * Created by sherryuan on 2019-01-26.
@@ -80,6 +86,9 @@ public class AudioRecorderActivity extends AppCompatActivity {
                 audioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
                 audioRecorder.setOutputFile(outputFile);
 
+                //Convert audio to .wav file
+                convert();
+
                 recordButton.setEnabled(true);
                 stopButton.setEnabled(false);
                 playButton.setEnabled(true);
@@ -105,5 +114,31 @@ public class AudioRecorderActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void convert(){
+        File flacFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp");
+        IConvertCallback callback = new IConvertCallback() {
+            @Override
+            public void onSuccess(File convertedFile) {
+                // So fast? Love it!
+            }
+            @Override
+            public void onFailure(Exception error) {
+                // Oops! Something went wrong
+            }
+        };
+        AndroidAudioConverter.with(this)
+                // Your current audio file
+                .setFile(flacFile)
+
+                // Your desired audio format
+                .setFormat(AudioFormat.MP3)
+
+                // An callback to know when conversion is finished
+                .setCallback(callback)
+
+                // Start conversion
+                .convert();
     }
 }
