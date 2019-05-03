e93e55f8ca202cbe8f50676b31585af8e701b95e
Sophie <berger.m.sophie@gmail.com>
2019-05-02 20:11:43 -0700

    preset periods added

--- app/src/main/java/com/squad/betakua/tap_neko/notifications/RefillReminder.java
+++ app/src/main/java/com/squad/betakua/tap_neko/notifications/RefillReminder.java
@@ -2,12 +2,12 @@ package com.squad.betakua.tap_neko.notifications;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.squad.betakua.tap_neko.R;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -15,30 +15,38 @@ import java.util.Date;
 public class RefillReminder extends AppCompatActivity {
 
     //Formatting the refill reminder date
-    Date reminderDate = Calendar.getInstance().getTime();
+    Date currentDate = new Date();
     Calendar calendar = Calendar.getInstance();
-    SimpleDateFormat reminderFormat = new SimpleDateFormat("dd-MMM-yyyy");
-    String reminderString = reminderFormat.format(reminderDate);
-
-
+    SimpleDateFormat reminderFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refill_reminder);
-//        TextView dateText = findViewById(R.id.dateText);
-//        Button twoWeeks = findViewById(R.id.twoWeeksButton);
-//
-//        if (twoWeeks.isPressed()){
-//            try {
-//                calendar.setTime(reminderFormat.parse(reminderString));
-//            } catch (ParseException e) {
-//                e.printStackTrace();
-//            }
-//            calendar.add(Calendar.DAY_OF_MONTH, 14);
-//            dateText.setText(reminderFormat.format(calendar.getTime()));
-//        }
+
+        TextView dateText = findViewById(R.id.dateText);
+        Button twoWeeks = findViewById(R.id.twoWeeksButton);
+        Button thirtyDays = findViewById(R.id.thirtyDaysButton);
+        Button ninetyDays = findViewById(R.id.ninetyDaysButton);
+
+        calendar.setTime(currentDate);
+
+        twoWeeks.setOnClickListener((View view) -> {
+            calendar.add(Calendar.DATE, 14);
+            Date reminderDate = calendar.getTime();
+            dateText.setText(reminderFormat.format(reminderDate));
+        });
+
+        thirtyDays.setOnClickListener((View view) -> {
+            calendar.add(Calendar.DATE, 30);
+            Date reminderDate = calendar.getTime();
+            dateText.setText(reminderFormat.format(reminderDate));
+        });
+
+        ninetyDays.setOnClickListener((View view) -> {
+            calendar.add(Calendar.DATE, 90);
+            Date reminderDate = calendar.getTime();
+            dateText.setText(reminderFormat.format(reminderDate));
+        });
     }
-
-
 }
