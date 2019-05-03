a940ce713a3bdf484ea7e024b87c9bb65b3fa0ac
Sophie <berger.m.sophie@gmail.com>
2019-05-02 20:40:40 -0700

    calendar implemented

--- app/src/main/java/com/squad/betakua/tap_neko/notifications/RefillReminder.java
+++ app/src/main/java/com/squad/betakua/tap_neko/notifications/RefillReminder.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import com.squad.betakua.tap_neko.R;
@@ -18,6 +19,8 @@ public class RefillReminder extends AppCompatActivity {
     Date currentDate = new Date();
     Calendar calendar = Calendar.getInstance();
     SimpleDateFormat reminderFormat = new SimpleDateFormat("dd/MM/yyyy");
+    Date reminderDate = new Date();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -28,24 +31,33 @@ public class RefillReminder extends AppCompatActivity {
         Button twoWeeks = findViewById(R.id.twoWeeksButton);
         Button thirtyDays = findViewById(R.id.thirtyDaysButton);
         Button ninetyDays = findViewById(R.id.ninetyDaysButton);
+        CalendarView calendarView = findViewById(R.id.calenderView);
 
         calendar.setTime(currentDate);
 
+        //set date to date selected on calendar, current date by default
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange( CalendarView view, int year, int month, int dayOfMonth) {
+                dateText.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+            }
+        });
+
         twoWeeks.setOnClickListener((View view) -> {
             calendar.add(Calendar.DATE, 14);
-            Date reminderDate = calendar.getTime();
+            reminderDate = calendar.getTime();
             dateText.setText(reminderFormat.format(reminderDate));
         });
 
         thirtyDays.setOnClickListener((View view) -> {
             calendar.add(Calendar.DATE, 30);
-            Date reminderDate = calendar.getTime();
+            reminderDate = calendar.getTime();
             dateText.setText(reminderFormat.format(reminderDate));
         });
 
         ninetyDays.setOnClickListener((View view) -> {
             calendar.add(Calendar.DATE, 90);
-            Date reminderDate = calendar.getTime();
+            reminderDate = calendar.getTime();
             dateText.setText(reminderFormat.format(reminderDate));
         });
     }
